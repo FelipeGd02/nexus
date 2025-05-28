2c4627edc2435bdaee2ea3824b5ffe273dc5bdb9
jjcrse <jjcrse452@gmmail.com>
2025-05-28 10:54:24 -0500

    Comment/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+//?  IMPORTACIONES CON SUS DETALLES PARA EL APP
+
+//&  Componentes de interfaz 
 import AppHeader from './components/header/Header';
 import { AppContainer } from './components/root/root';
+
+//!  Pantallas principales 
 import AuthScreen from './screens/auth/AuthScreen';
 import CategoriesScreen from './screens/categories/CategoriesScreen';
 import CommunityScreen from './screens/community/CommunityScreen';
@@ -7,14 +12,19 @@ import LandingScreen from './screens/landing/LandingScreen';
 import ProfileScreen from './screens/profile/ProfileScreen';
 import ThreadDetailScreen from './screens/threadsdetail/ThreadDetailScreen';
 import ThreadsScreen from './screens/threads/ThreadsScreen';
+
+//*  Componentes específicos 
 import EditProfileModal from './components/profile/EditProfileModal';
 import CreatePost from './components/post/CreatePost';
 
+//*  INICIO DE LA APLICACIÓN 
 console.log('App started');
 
-// Register all custom elements
+//& REGISTRO DE CUSTOM ELEMENTS 
 customElements.define('app-container', AppContainer);
 customElements.define('app-header', AppHeader);
+
+//!  Pantallas 
 customElements.define('auth-screen', AuthScreen);
 customElements.define('landing-screen', LandingScreen);
 customElements.define('threads-screen', ThreadsScreen);
@@ -22,5 +32,7 @@ customElements.define('thread-detail-screen', ThreadDetailScreen);
 customElements.define('categories-screen', CategoriesScreen);
 customElements.define('community-screen', CommunityScreen);
 customElements.define('profile-screen', ProfileScreen);
+
+//!  Componentes funcionales 
 customElements.define('edit-profile-modal', EditProfileModal);
 customElements.define('create-post', CreatePost);
