84da825e19c3fb84df3849d8c470da3b3b42eade
jjcrse <jjcrse452@gmmail.com>
2025-05-27 21:41:16 -0500

    Style/assets/solid.svg

--- src/services/Feed.ts
+++
@@ -1,8 +0,0 @@
-// async connectedCallback() {
-//     const response = await fetch('./data.json');
-//     const data = await response.json();
-
-//     this.render(data);
-//   }
-
-// exportar por una función
